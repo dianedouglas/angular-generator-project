db08e175847a919080860a96e3430f0d4093a92b
Diane Douglas <diane.douglas1@gmail.com>
2016-01-14 16:45:02 -0800

    generate 'dianeproject' with 'showpeople' component.

---
+++ src/app/components/showpeople/showpeople.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+  selector: 'showpeople',
+  templateUrl: 'app/components/showpeople/showpeople.html',
+  styleUrls: ['app/components/showpeople/showpeople.css'],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+export class Showpeople {
+
+  constructor() {}
+
+}
